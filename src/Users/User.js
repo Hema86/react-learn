3573dcdb5d26f80a83e9fb91d831036e372fd6cf
Hema86 <hemakempur@gmail.com>
2019-06-26 12:13:24 +0530

    state and props

--- src/Users/User.js
+++ src/Users/User.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 export default function User (props) {
-  let age = props.age ? props.age : 'NA'
   return (
     <div>
-      <div>Name: {props.children} | age: {age}</div>
+      <div>Name: {props.children} | age: {props.age}</div>
     </div>
   )
 }
--- src/Users/Users.js
+++ src/Users/Users.js
@@ -2,13 +2,38 @@ import React, { Component } from 'react'
 import User from './User'
 
 export default class Users extends Component {
+  state = {
+    users: [
+      {name:'John', age: 20},
+      {name:'Jill', age: 30},
+      {name:'Peter', age: 40}
+    ],
+    title:'Users List'
+  }
+
+  makeMeYounger = () => {
+
+    const newUser = this.state.users.map(user => {
+      const tempUser=user
+      if(tempUser.age > 10) {
+      tempUser.age -=10
+      }
+      return tempUser
+    })
+    this.setState({
+      users: newUser
+    })
+  }
   render () {
     return (
       <div>
-        <h1>{this.props.title}</h1>
-        <User >John</User>
-        <User age='30'>Jill</User>
-        <User age='40'>Peter</User>
+        <button onClick={this.makeMeYounger}>make us 10 year younger</button>
+        <h1>{this.state.title}</h1>
+       {
+         this.state.users.map(user => {
+           return <User age={user.age}>{user.name}</User>
+         })
+       }
       </div>
     )
   }
